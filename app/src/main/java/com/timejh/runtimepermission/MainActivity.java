42c72bd929b7c5f0e2d839398acddbfa488825f2
timejp <tokijh@naver.com>
2017-02-01 11:33:13 +0900

    Runtime Permission Check

--- app/src/main/java/com/timejh/runtimepermission/MainActivity.java
+++ app/src/main/java/com/timejh/runtimepermission/MainActivity.java
@@ -1,13 +1,50 @@
 package com.timejh.runtimepermission;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQ_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        } else {
+            loadData();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermission() {
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            String permArr[] = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permArr, REQ_CODE);
+        } else {
+            loadData();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQ_CODE) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadData();
+            }
+        }
+    }
+
+    private void loadData() {
+        Toast.makeText(this, "프로그램 로드완료", Toast.LENGTH_SHORT).show();
     }
 }
